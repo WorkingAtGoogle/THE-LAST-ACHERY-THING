825a468a6542800474af786845c6368c7ddcdadf
Vihar <89672633+WorkingAtGoogle@users.noreply.github.com>
2023-03-25 09:47:01 +0800

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,26 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("https://o.remove.bg/downloads/f2ba03fb-b582-4046-851b-d59c6278bd2b/image-removebg-preview.png");
+
+    World.add(world, this.body);
+  }
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
